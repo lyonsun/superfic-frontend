1d0d150f7876ee50a9777caf3e6ea61d9a673668
Liang Sun <sunly917@gmail.com>
2022-04-06 15:08:39 +0300

    add pre loader to home/posts page. update README.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# [Superfic Frontend]
+# Superfic Frontend
 
 ## Pre Setup
 
@@ -10,6 +10,8 @@
 
 ## Local Development
 
+Run `yarn install` to install all dependencies.
+
 ### With Docker Compose
 
 > Follow the instructions in the [README.md]() file.
@@ -20,6 +22,28 @@ To hot reload the frontend in development, simply run:
 
     yarn dev
 
-## View the website
+### View the website
 
 Visit http://localhost:3000/ in your browser.
+
+## Deployment
+
+### To Vercel
+
+1. [Sign in to Vercel](https://vercel.com/login)
+
+2. [Create a new project](https://vercel.com/new)
+
+3. Connect your GitHub repository of this project to the project you created on Vercel.
+
+4. Add new environment variable `NEXT_PUBLIC_BACKEND_URL` to your project, with the URL of the backend application, e.g. `https://superfic-backend.herokuapp.com`.
+
+5. Make any changes to the frontend application.
+
+6. Any push to the `main` branch will automatically deploy the latest version of the frontend application to Vercel.
+
+7. You can then view the website with the URL given by Vercel.
+
+### Live deployment on Vercel
+
+You can find my Vercel deployment of this project [here](https://superfic-frontend.vercel.app/).
--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,8 @@ import { getPosts, getPostsCount } from './api/posts'
 const Home = () => {
   const router = useRouter()
 
+  const [loaded, setLoaded] = useState(false)
+
   const [posts, setPosts] = useState([])
   const [totalCount, setTotalCount] = useState(0)
 
@@ -31,6 +33,10 @@ const Home = () => {
 
     fetchPosts()
     fetchPostsCount()
+
+    setTimeout(() => {
+      setLoaded(true)
+    }, 500)
   }, [pageNumber, pageCount])
 
   // Handle pagination
@@ -46,51 +52,68 @@ const Home = () => {
     })
   }
 
-  return posts.length > 0 ?
-    (
-      <Layout>
-        <h1 className="text-4xl my-8 border-b">All Posts</h1>
+  const loadingClass = loaded ? 'hidden' : 'visible'
+  const loadingPlaceholder = (
+    <div className={`loading text-4xl my-8 ${loadingClass}`}>
+      Loading...
+    </div>
+  )
 
-        <div className="posts flex flex-col gap-8 mb-10">
-          {
-            posts.map(post => {
-              return (
-                <div key={post.id} className="relative rounded p-6 shadow-lg hover:shadow-xl border-l-8 border-l-indigo-600">
-                  <div className="mb-4 pb-2 border-b flex justify-between items-center">
-                    <p className="text-fuchsia-600 font-bold">{post.user_name}</p>
-                    <p className="text-sm text-gray-400">{post.created_time.substring(0, 10)}</p>
-                  </div>
-                  <p className="">{post.message}</p>
+  const postClass = loaded && posts.length ? 'visible' : 'hidden'
+  const postsContainer = (
+    <div className={`${postClass}`}>
+      <h1 className="text-4xl my-8 border-b">All Posts</h1>
+
+      <div className="posts flex flex-col gap-8 mb-10">
+        {
+          posts.map(post => {
+            return (
+              <div key={post.id} className="relative rounded p-6 shadow-lg hover:shadow-xl border-l-8 border-l-indigo-600">
+                <div className="mb-4 pb-2 border-b flex justify-between items-center">
+                  <p className="text-fuchsia-600 font-bold">{post.user_name}</p>
+                  <p className="text-sm text-gray-400">{post.created_time.substring(0, 10)}</p>
                 </div>
-              )
-            })
-          }
-        </div>
+                <p className="">{post.message}</p>
+              </div>
+            )
+          })
+        }
+      </div>
 
-        <ReactPaginate
-          previousLabel={'<'}
-          nextLabel={'>'}
-          breakLabel={'...'}
-          containerClassName={'pagination mb-10 flex justify-center items-center gap-4 text-md'}
-          pageLinkClassName={'page-item px-2 md:px-4 py-1 md:py-2 hover:bg-indigo-700 hover:text-white'}
-          activeLinkClassName={'active bg-indigo-600 text-white font-bold hover:bg-indigo-700'}
-          breakClassName={'break-me'}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={2}
-          pageCount={totalCount / pageCount}
-          onPageChange={paginationHandler}
-        />
-      </Layout>
-    ) :
-    (
-      <Layout>
-        <h1 className="text-4xl my-8 border-b">All Posts</h1>
-        <div className='not-found text-center'>
-          <Image src='/images/not-found.png' width={500} height={500} objectFit="contain" alt='posts not found' />
-          <p className='text-2xl text-gray-400 mb-8'>No Posts Found</p>
-        </div>
-      </Layout>
-    )
+      <ReactPaginate
+        previousLabel={'<'}
+        nextLabel={'>'}
+        breakLabel={'...'}
+        containerClassName={'pagination mb-10 flex justify-center items-center gap-4 text-md'}
+        pageLinkClassName={'page-item px-2 md:px-4 py-1 md:py-2 hover:bg-indigo-700 hover:text-white'}
+        activeLinkClassName={'active bg-indigo-600 text-white font-bold hover:bg-indigo-700'}
+        breakClassName={'break-me'}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={2}
+        pageCount={totalCount / pageCount}
+        onPageChange={paginationHandler}
+      />
+    </div>
+  )
+
+  const notFoundClass = loaded && !posts.length ? 'visible' : 'hidden'
+  const notFoundContainer = (
+    <div className={`${notFoundClass}`}>
+      <h1 className="text-4xl my-8 border-b">All Posts</h1>
+      <div className='not-found text-center'>
+        <Image src='/images/not-found.png' width={500} height={500} objectFit="contain" alt='posts not found' />
+        <p className='text-2xl text-gray-400 mb-8'>No Posts Found</p>
+      </div>
+    </div>
+  )
+
+  const pageContainer = !loaded ? loadingPlaceholder : posts.length ? postsContainer : notFoundContainer
+
+  return (
+    <Layout>
+      {pageContainer}
+    </Layout>
+  )
 }
 
 export default Home
